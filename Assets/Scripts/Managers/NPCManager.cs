652bb56a6b60fd9f4b2fc7550a50c9220f554381
Naren Keshav <narenkeshav@yahoo.com>
2014-05-18 18:06:43 -0400

    no message

---
+++ Assets/Scripts/Managers/NPCManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCManager : MonoBehaviour
+{
+	// Responsibilities
+	/*
+		1. Co-ordinates between GameManager & Enemies in a particular scene
+		2. 
+	 */
+
+
+	public GameObject[] EnemyUnits;
+
+
+}
