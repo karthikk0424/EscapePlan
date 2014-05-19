e428740f3fdd974c596d4c1fc55314c3ae631134
karthik karunakaran <karthikk0424@gmail.com>
2014-05-19 18:42:39 -0400

    [MenuMenu]

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	private void OnGUI() 
+	{
+		if (GUI.Button(new Rect(200, 100, 150, 50), "Ready To Escape"))
+		{
+			Application.LoadLevel(1);
+		}
+	}
+}
