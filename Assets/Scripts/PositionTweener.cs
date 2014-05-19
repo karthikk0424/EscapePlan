2edc888814b58bd6626632c43d3c5ca20d7c3481
Naren Keshav <narenkeshav@yahoo.com>
2014-05-18 23:39:14 -0400

    1. Changed the tweening functionality. It has more flexibility.

--- Assets/Scripts/PositionTweener.cs
+++ Assets/Scripts/PositionTweener.cs
@@ -11,25 +11,59 @@ public class PositionTweener : MonoBehaviour
 {	
 	public Vector2 TweenFrom;
 	public Vector2 TweenTo;
-	public float Speed = 0.25f;
+	public float TimeToMove = 4;
+	//public float Speed = 0.25f;
 	public bool ResetOnLoad = false;
+
+	public enum EasingType
+	{
+		EaseIn,
+		EaseOut, 
+		EaseInOut
+	};
+	public EasingType ThisType = EasingType.EaseOut;
 
 	public void PlayAnimation()
 	{
-		StartCoroutine(StartAnimation());
+		this.StartCoroutine(StartAnimation());
 	}
 
-	IEnumerator  StartAnimation()
+	private IEnumerator StartAnimation()
 	{
-
 		float lastTime = Time.timeSinceLevelLoad;
 		float timer = 0;
+		TimeToMove = (1/TimeToMove);
 		while ((timer < 1))
 		{
-			transform.localPosition = Vector2.Lerp(TweenFrom, TweenTo, timer);
-			timer += ((Time.timeSinceLevelLoad - lastTime) * Speed);
+			transform.localPosition = Vector2.Lerp(TweenFrom, TweenTo, EaseTheTimer(timer));
+			timer += ((Time.timeSinceLevelLoad - lastTime) * TimeToMove);
 			lastTime = Time.timeSinceLevelLoad;
 			yield return null;
 		}
 	}
+
+	//http://theinstructionlimit.com/wp-content/uploads/2009/07/Easing.cs
+	private float EaseTheTimer(float param)
+	{
+		switch(ThisType)
+		{
+			case EasingType.EaseIn:
+				return	Mathf.Sin(param * (Mathf.PI/2)  - (Mathf.PI/2)) + 1;
+				break;
+
+			case EasingType.EaseOut:
+				return	Mathf.Sin(param * (Mathf.PI/2));
+				break;
+
+			case EasingType.EaseInOut:
+				return	(Mathf.Sin(param * (Mathf.PI/2) - (Mathf.PI/2)) + 1) / 2;	
+				break;
+	
+			default:
+				return param;
+				break;
+		}
+	}
+
+
 }
