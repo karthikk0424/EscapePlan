2df1959dafaee909c8b5d743ceb765e46be5c35a
Naren Keshav <narenkeshav@yahoo.com>
2014-05-19 22:33:27 -0400

    1. Main Menu UI has been implemented. It works for both loading first level and exiting the application.

---
+++ Assets/Scripts/MainMenu/MainMenuInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuInput : MonoBehaviour 
+{
+	public enum TypeOfButton
+	{
+		None,
+		EnterGame, 
+		ExitGame
+	}
+	public TypeOfButton ThisButton;
+
+	private void OnMouseDown()
+	{
+		switch(ThisButton)
+		{
+			case TypeOfButton.EnterGame:
+				Application.LoadLevel(1);
+				break;
+
+			case TypeOfButton.ExitGame:
+				Application.Quit();
+				break;
+		}
+	}
+}
